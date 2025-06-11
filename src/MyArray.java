d94d770c93ce3f9839726614e5e16e1e8af174af
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-06-11 12:48:42 +0430

    insert at

--- src/MyArray.java
+++ src/MyArray.java
@@ -82,6 +82,25 @@ public class MyArray {
         }
     }
 
+    public void insertAt(int item, int index) {
+        if (index < 0 || index > count)
+            throw new IllegalArgumentException();
+
+        if (count == items.length) {
+            int[] newItems = new int[count * 2];
+            for (int i = 0; i < count; i++)
+                newItems[i] = items[i];
+            items = newItems;
+        }
+
+        for (int i = count - 1; i >= index; i--) {
+            items[i + 1] = items[i];
+        }
+
+        items[index] = item;
+        count++;
+    }
+
 
     public void print() { //print method
         for (int i = 0; i < count; i++) {
