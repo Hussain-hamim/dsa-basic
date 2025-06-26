607ad9e2638e35586bd9e0f8a87c3c8cd7c0873d
Hussain Hamim <mohammadhussainafghan83@gmail.com>
2025-06-26 21:32:47 +0430

    hashtable: put method

--- src/HashTable.java
+++ src/HashTable.java
@@ -4,16 +4,25 @@ public class HashTable {
     private LinkedList<Entry>[] entries = new LinkedList[5];
 
     public void put(int key, String value) {
-        var index = hash(key);
-          if(entries[index] === null)
-              entries[index] = new LinkedList<>();
+        var index = hash(key); // key where we should store our key value pair
+        if (entries[index] == null) {
+            entries[index] = new LinkedList<>();
+        }
 
-          var entry = new Entry(key, value);
-          entries[index].addLast(entry);
+        var bucket = entries[index];
+        for (var entry : bucket) {
+            if (entry.key == key) {
+                entry.value = value;
+                return;
+            }
+        }
+
+        var entry = new Entry(key, value); // we wrap the object in entry object
+        bucket.addLast(entry); // get the linkedlist at index position and add the entry object
     }
 
     private int hash(int key) {
-        return key % entries.length;
+        return key % entries.length; // whatever key we have we reduce it to the size of our array
     }
 
 
